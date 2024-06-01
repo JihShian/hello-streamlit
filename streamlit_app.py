df6282297bd9f2e960ac750ae49391a3f69b8a28
JihShian <50051633+JihShian@users.noreply.github.com>
2024-06-01 20:51:53 +0000

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,157 +1,306 @@
+import joblib
+from sklearn.ensemble import RandomForestRegressor
 import streamlit as st
-import numpy as np
 import pandas as pd
-import altair as alt
-from datetime import datetime, timedelta
+from xgboost import XGBRegressor
+from sklearn.preprocessing import LabelEncoder
 
 # Page title
-st.set_page_config(page_title='Support Ticket Workflow', page_icon='🎫')
-st.title('🎫 Support Ticket Workflow')
-st.info('To write a ticket, fill out the form below. Check status or review ticketing analytics using the tabs below.')
+st.set_page_config(page_title='Rental Price Prediction')
+st.title('Rental Price Prediction')
+st.info('This is a rental price prediction mainly based in KL & Selangor area. Fill in the information and check the predicted price below.')
 
+# Load data
+@st.cache_data
+def load_data():
+    return pd.read_csv('cleaned_data.csv').copy()  # Make a copy of the DataFrame to avoid mutation
 
-# Generate data
-## Set seed for reproducibility
-np.random.seed(42)
+# Function to map 'Yes' or 'No' to 1 or 0
+def map_yes_no_to_binary(value):
+    return 1 if value == 'Yes' else 0
 
-## Function to generate a random issue description
-def generate_issue():
-    issues = [
-        "Network connectivity issues in the office",
-        "Software application crashing on startup",
-        "Printer not responding to print commands",
-        "Email server downtime",
-        "Data backup failure",
-        "Login authentication problems",
-        "Website performance degradation",
-        "Security vulnerability identified",
-        "Hardware malfunction in the server room",
-        "Employee unable to access shared files",
-        "Database connection failure",
-        "Mobile application not syncing data",
-        "VoIP phone system issues",
-        "VPN connection problems for remote employees",
-        "System updates causing compatibility issues",
-        "File server running out of storage space",
-        "Intrusion detection system alerts",
-        "Inventory management system errors",
-        "Customer data not loading in CRM",
-        "Collaboration tool not sending notifications"
-    ]
-    return np.random.choice(issues)
+# Define region bins
+region_bins = {
+    'Kuala Lumpur': 0,
+    'Selangor': 1
+}
 
-## Function to generate random dates
-start_date = datetime(2023, 6, 1)
-end_date = datetime(2023, 12, 20)
-id_values = ['TICKET-{}'.format(i) for i in range(1000, 1100)]
-issue_list = [generate_issue() for _ in range(100)]
+# Define furnished bins
+furnished_bins = {
+    'Not Furnished': 0,
+    'Partially Furnished': 1,
+    'Fully Furnished': 2
+}
 
+# Define a dictionary that maps each location to its corresponding region
+location_to_region = {
+    'Cheras': 'Selangor',
+    'Taman Desa': 'Kuala Lumpur',
+    'Sentul': 'Kuala Lumpur',
+    'Mont Kiara': 'Kuala Lumpur',
+    'Setapak': 'Kuala Lumpur',
+    'Ampang': 'Selangor',
+    'Segambut': 'Kuala Lumpur',
+    'Desa ParkCity': 'Kuala Lumpur',
+    'Bukit Jalil': 'Kuala Lumpur',
+    'Kepong': 'Kuala Lumpur',
+    'Wangsa Maju': 'Kuala Lumpur',
+    'Jalan Kuching': 'Kuala Lumpur',
+    'Bandar Menjalara': 'Kuala Lumpur',
+    'Old Klang Road': 'Kuala Lumpur',
+    'Desa Pandan': 'Kuala Lumpur',
+    'KLCC': 'Kuala Lumpur',
+    'Ampang Hilir': 'Kuala Lumpur',
+    'Bukit Bintang': 'Kuala Lumpur',
+    'KL City': 'Kuala Lumpur',
+    'Jalan Ipoh': 'Kuala Lumpur',
+    'Setiawangsa': 'Kuala Lumpur',
+    'Gombak': 'Selangor',
+    'Sungai Besi': 'Kuala Lumpur',
+    'Jinjang': 'Kuala Lumpur',
+    'Sri Petaling': 'Kuala Lumpur',
+    'Bangsar South': 'Kuala Lumpur',
+    'Pantai': 'Kuala Lumpur',
+    'Brickfields': 'Kuala Lumpur',
+    'Kuchai Lama': 'Kuala Lumpur',
+    'Jalan Sultan Ismail': 'Kuala Lumpur',
+    'Bangsar': 'Kuala Lumpur',
+    'Pandan Indah': 'Kuala Lumpur',
+    'Pandan Jaya': 'Kuala Lumpur',
+    'Damansara Heights': 'Kuala Lumpur',
+    'Bandar Damai Perdana': 'Kuala Lumpur',
+    'Titiwangsa': 'Kuala Lumpur',
+    'Bandar Tasik Selatan': 'Kuala Lumpur',
+    'Pandan Perdana': 'Kuala Lumpur',
+    'Keramat': 'Kuala Lumpur',
+    'Pudu': 'Kuala Lumpur',
+    'OUG': 'Kuala Lumpur',
+    'Taman Tun Dr Ismail': 'Kuala Lumpur',
+    'Sri Hartamas': 'Kuala Lumpur',
+    'Solaris Dutamas': 'Kuala Lumpur',
+    'Puchong': 'Selangor',
+    'Seputeh': 'Kuala Lumpur',
+    'Sri Damansara': 'Kuala Lumpur',
+    'Taman Melawati': 'Kuala Lumpur',
+    'Desa Petaling': 'Kuala Lumpur',
+    'Others': 'Kuala Lumpur',
+    'Serdang': 'Selangor',
+    'City Centre': 'Kuala Lumpur',
+    'Salak Selatan': 'Kuala Lumpur',
+    'Sungai Penchala': 'Kuala Lumpur',
+    'Mid Valley City': 'Kuala Lumpur',
+    'Damansara': 'Kuala Lumpur',
+    'Cyberjaya': 'Selangor',
+    'Shah Alam': 'Selangor',
+    'Klang': 'Selangor',
+    'Petaling Jaya': 'Selangor',
+    'Subang Jaya': 'Selangor',
+    'Bandar Sunway': 'Selangor',
+    'Seri Kembangan': 'Selangor',
+    'Kajang': 'Selangor',
+    'Rawang': 'Selangor',
+    'Kota Damansara': 'Selangor',
+    'Batu Caves': 'Selangor',
+    'Semenyih': 'Selangor',
+    'Bukit Jelutong': 'Selangor',
+    'USJ': 'Selangor',
+    'Damansara Damai': 'Selangor',
+    'Bandar Mahkota Cheras': 'Selangor',
+    'Puncak Alam': 'Selangor',
+    'Sepang': 'Selangor',
+    'Kuala Langat': 'Selangor',
+    'Setia Alam': 'Selangor',
+    'Selayang': 'Selangor',
+    'Sungai Buloh': 'Selangor',
+    'Bangi': 'Selangor',
+    'Dengkil': 'Selangor',
+    'Ara Damansara': 'Selangor',
+    'I-City': 'Selangor',
+    'Bandar Sri Damansara': 'Selangor',
+    'Damansara Perdana': 'Selangor',
+    'Bandar Saujana Putra': 'Selangor',
+    'Kota Kemuning': 'Selangor',
+    'Ulu Klang': 'Selangor',
+    'Kapar': 'Selangor',
+    'Balakong': 'Selangor',
+    'Bandar Sungai Long': 'Selangor',
+    'Port Klang': 'Selangor',
+    'Hulu Langat': 'Selangor',
+    'Bandar Kinrara': 'Selangor',
+    'Jenjarom': 'Selangor',
+    'Glenmarie': 'Selangor',
+    'Kelana Jaya': 'Selangor',
+    'Puchong South': 'Selangor',
+    'Alam Impian': 'Selangor',
+    'Pulau Indah (Pulau Lumut)': 'Selangor',
+    'Bandar Bukit Tinggi': 'Selangor',
+    'Putra Heights': 'Selangor',
+    'Saujana Utama': 'Selangor',
+    'Bandar Bukit Raja': 'Selangor',
+    'Bandar Utama': 'Selangor',
+    'Subang Bestari': 'Selangor',
+    'Bandar Botanic': 'Selangor',
+    'Banting': 'Selangor',
+    'Kuala Selangor': 'Selangor',
+    'Salak Tinggi': 'Selangor',
+    'Serendah': 'Selangor',
+    'Bukit Beruntung': 'Selangor',
+    'Mutiara Damansara': 'Selangor',
+    'Telok Panglima Garang': 'Selangor',
+    'Bukit Subang': 'Selangor',
+    'Puncak Jalil': 'Selangor'
+}
 
-def generate_random_dates(start_date, end_date, id_values):
-    date_range = pd.date_range(start_date, end_date).strftime('%m-%d-%Y')
-    return np.random.choice(date_range, size=len(id_values), replace=False)
+# Define location bins
+location_bins = {
+    'Alam Impian': 5,
+    'Ampang': 3,
+    'Ampang Hilir': 6,
+    'Ara Damansara': 6,
+    'Balakong': 2,
+    'Bandar Botanic': 2,
+    'Bandar Bukit Raja': 3,
+    'Bandar Bukit Tinggi': 6,
+    'Bandar Damai Perdana': 4,
+    'Bandar Kinrara': 3,
+    'Bandar Mahkota Cheras': 4,
+    'Bandar Menjalara': 4,
+    'Bandar Saujana Putra': 3,
+    'Bandar Sri Damansara': 1,
+    'Bandar Sungai Long': 1,
+    'Bandar Sunway': 4,
+    'Bandar Tasik Selatan': 1,
+    'Bandar Utama': 4,
+    'Bangi': 2,
+    'Bangsar': 6,
+    'Bangsar South': 5,
+    'Banting': 1,
+    'Batu Caves': 2,
+    'Brickfields': 6,
+    'Bukit Beruntung': 1,
+    'Bukit Bintang': 6,
+    'Bukit Jalil': 3,
+    'Bukit Jelutong': 5,
+    'Bukit Subang': 1,
+    'Cheras': 3,
+    'City Centre': 6,
+    'Cyberjaya': 3,
+    'Damansara': 6,
+    'Damansara Damai': 2,
+    'Damansara Heights': 4,
+    'Damansara Perdana': 3,
+    'Dengkil': 2,
+    'Desa Pandan': 5,
+    'Desa ParkCity': 6,
+    'Desa Petaling': 2,
+    'Glenmarie': 5,
+    'Gombak': 4,
+    'Hulu Langat': 1,
+    'I-City': 4,
+    'Jalan Ipoh': 5,
+    'Jalan Kuching': 5,
+    'Jalan Sultan Ismail': 6,
+    'Jenjarom': 2,
+    'Jinjang': 1,
+    'KL City': 6,
+    'KLCC': 6,
+    'Kajang': 2,
+    'Kapar': 1,
+    'Kelana Jaya': 5,
+    'Kepong': 3,
+    'Keramat': 5,
+    'Klang': 3,
+    'Kota Damansara': 5,
+    'Kota Kemuning': 6,
+    'Kuala Langat': 3,
+    'Kuala Selangor': 1,
+    'Kuchai Lama': 4,
+    'Mid Valley City': 6,
+    'Mont Kiara': 6,
+    'Mutiara Damansara': 2,
+    'OUG': 3,
+    'Old Klang Road': 5,
+    'Others': 3,
+    'Pandan Indah': 2,
+    'Pandan Jaya': 2,
+    'Pandan Perdana': 4,
+    'Pantai': 4,
+    'Petaling Jaya': 3,
+    'Port Klang': 4,
+    'Puchong': 2,
+    'Puchong South': 2,
+    'Pudu': 5,
+    'Pulau Indah (Pulau Lumut)': 2,
+    'Puncak Alam': 1,
+    'Puncak Jalil': 1,
+    'Putra Heights': 2,
+    'Rawang': 1,
+    'Salak Selatan': 1,
+    'Salak Tinggi': 1,
+    'Saujana Utama': 2,
+    'Segambut': 5,
+    'Selayang': 3,
+    'Semenyih': 1,
+    'Sentul': 4,
+    'Sepang': 2,
+    'Seputeh': 5,
+    'Serdang': 1,
+    'Serendah': 1,
+    'Seri Kembangan': 2,
+    'Setapak': 4,
+    'Setia Alam': 4,
+    'Setiawangsa': 5,
+    'Shah Alam': 3,
+    'Solaris Dutamas': 5,
+    'Sri Damansara': 6,
+    'Sri Hartamas': 5,
+    'Sri Petaling': 4,
+    'Subang Bestari': 2,
+    'Subang Jaya': 3,
+    'Sungai Besi': 4,
+    'Sungai Buloh': 3,
+    'Sungai Penchala': 1,
+    'Taman Desa': 4,
+    'Taman Melawati': 6,
+    'Taman Tun Dr Ismail': 6,
+    'Telok Panglima Garang': 5,
+    'Titiwangsa': 6,
+    'USJ': 3,
+    'Ulu Klang': 6,
+    'Wangsa Maju': 4
+}
 
-## Generate 100 rows of data
-data = {'Issue': issue_list,
-        'Status': np.random.choice(['Open', 'In Progress', 'Closed'], size=100),
-        'Priority': np.random.choice(['High', 'Medium', 'Low'], size=100),
-        'Date Submitted': generate_random_dates(start_date, end_date, id_values)
-    }
-df = pd.DataFrame(data)
-df.insert(0, 'ID', id_values)
-df = df.sort_values(by=['Status', 'ID'], ascending=[False, False])
+data = load_data()
+label_encoder = LabelEncoder()
+label_encoder.fit(data['property_type'])
 
-## Create DataFrame
-if 'df' not in st.session_state:
-    st.session_state.df = df
+# Train the model
+model = joblib.load('RF_model.pkl')
 
-# Sort dataframe
-def sort_df():
-    st.session_state.df = edited_df.copy().sort_values(by=['Status', 'ID'], ascending=[False, False])
+# Rental Price prediction form
+with st.form('predict'):
+    location = st.selectbox('Location', list(location_bins.keys()))
+    region = st.selectbox('Region', list(region_bins.keys()))
+    property_type = st.selectbox('Property Type',['Apartment', 'Condominium', 'Duplex' ,'Flat', 'Service Residence', 'Studio', 'Townhouse Condo', 'Others'])
+    rooms = st.selectbox('Rooms Number',['1', '2','3','4','5','6','7','8','9','10'])
+    size = st.number_input('Size (sqft)')
+    furnished = st.selectbox('Furnished',list(furnished_bins.keys()))
+    gymnasium = st.radio("Gymnasium", ("Yes", "No"))
+    air_cond = st.radio("Air-cond", ("Yes", "No"))
+    washing_machine = st.radio("Washing Machine", ("Yes", "No"))
+    swimming_pool = st.radio("Swimming Pool", ("Yes", "No"))
+    submit = st.form_submit_button('Predict')
 
-
-# Tabs for app layout
-tabs = st.tabs(['Write a ticket', 'Ticket Status and Analytics'])
-
-recent_ticket_number = int(max(st.session_state.df.ID).split('-')[1])
-
-with tabs[0]:
-  with st.form('addition'):
-    issue = st.text_area('Description of issue')
-    priority = st.selectbox('Priority', ['High', 'Medium', 'Low'])
-    submit = st.form_submit_button('Submit')
-
-  if submit:
-      today_date = datetime.now().strftime('%m-%d-%Y')
-      df2 = pd.DataFrame([{'ID': f'TICKET-{recent_ticket_number+1}',
-                           'Issue': issue,
-                           'Status': 'Open',
-                           'Priority': priority,
-                           'Date Submitted': today_date
-                          }])
-      st.write('Ticket submitted!')
-      st.dataframe(df2, use_container_width=True, hide_index=True)
-      st.session_state.df = pd.concat([st.session_state.df, df2], axis=0).sort_values(by=['Status', 'ID'], ascending=[False, False])
-
-with tabs[1]:
-  status_col = st.columns((3,1))
-  with status_col[0]:
-      st.subheader('Support Ticket Status')
-  with status_col[1]:
-      st.write(f'No. of tickets: `{len(st.session_state.df)}`')
-
-  st.markdown('**Things to try:**')
-  st.info('1️⃣ Update Ticket **Status** or **Priority** and see how plots are updated in real-time!')
-  st.success('2️⃣ Change values in **Status** column from *"Open"* to either *"In Progress"* or *"Closed"*, then click on the **Sort DataFrame by the Status column** button to see the refreshed DataFrame with the sorted **Status** column.')
-
-  edited_df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212,
-                column_config={'Status': st.column_config.SelectboxColumn(
-                                            'Status',
-                                            help='Ticket status',
-                                            options=[
-                                                'Open',
-                                                'In Progress',
-                                                'Closed'
-                                            ],
-                                            required=True,
-                                            ),
-                               'Priority': st.column_config.SelectboxColumn(
-                                           'Priority',
-                                            help='Priority',
-                                            options=[
-                                                'High',
-                                                'Medium',
-                                                'Low'
-                                            ],
-                                            required=True,
-                                            ),
-                             })
-  st.button('🔄 Sort DataFrame by the Status column', on_click=sort_df)
-  
-  # Status plot
-  st.subheader('Support Ticket Analytics')
-  col = st.columns((1,3,1))
-    
-  with col[0]:
-      n_tickets_queue = len(st.session_state.df[st.session_state.df.Status=='Open'])
-      
-      st.metric(label='First response time (hr)', value=5.2, delta=-1.5)
-      st.metric(label='No. of tickets in the queue', value=n_tickets_queue, delta='')
-      st.metric(label='Avg. ticket resolution time (hr)', value=16, delta='')
-      
-      
-  with col[1]:
-      status_plot = alt.Chart(edited_df).mark_bar().encode(
-          x='month(Date Submitted):O',
-          y='count():Q',
-          xOffset='Status:N',
-          color = 'Status:N'
-      ).properties(title='Ticket status in the past 6 months', height=300).configure_legend(orient='bottom', titleFontSize=14, labelFontSize=14, titlePadding=5)
-      st.altair_chart(status_plot, use_container_width=True, theme='streamlit')
-      
-  with col[2]:
-      priority_plot = alt.Chart(edited_df).mark_arc().encode(
-                          theta="count():Q",
-                          color="Priority:N"
-                      ).properties(title='Current ticket priority', height=300).configure_legend(orient='bottom', titleFontSize=14, labelFontSize=14, titlePadding=5)
-      st.altair_chart(priority_plot, use_container_width=True, theme='streamlit')
+if submit:
+    property_type = int(label_encoder.transform([property_type])[0])
+    rooms = int(rooms)
+    gymnasium = map_yes_no_to_binary(gymnasium)
+    air_cond = map_yes_no_to_binary(air_cond)
+    washing_machine = map_yes_no_to_binary(washing_machine)
+    swimming_pool = map_yes_no_to_binary(swimming_pool)
+    location_bin = location_bins[location]
+    furnished = furnished_bins[furnished]
+    region = region_bins[region]
+    input_data = [[property_type, rooms, size, furnished, region, gymnasium, air_cond, washing_machine, swimming_pool, location_bin]]
+    prediction = model.predict(input_data)
+    st.write("Predicted Rental Price:", round(prediction[0], 2))
